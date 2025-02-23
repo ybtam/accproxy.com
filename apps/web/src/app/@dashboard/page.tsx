ed4176d6704c120d2423f507e10758e74dede0da
Yi Tam <tam.yi3d@gmail.com>
2025-02-23 20:59:52 +0100

    add logout button

--- apps/web/src/app/@dashboard/page.tsx
+++ apps/web/src/app/@dashboard/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import {LogoutButton} from "@/app/@dashboard/_components/logout-button";
 
 export default function Login() {
   return <div>
     <h1>Auth</h1>
     <Link href={'/expenses'}>Expenses</Link>
+    <LogoutButton/>
   </div>
 }
