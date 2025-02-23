feb8c11f4868a19f9082d08238b7ff18da43ba1f
Yi Tam <tam.yi3d@gmail.com>
2025-02-23 13:59:19 +0100

    return schema for authorized and non authorized

--- apps/api/src/utils/schema-builder.ts
+++ apps/api/src/utils/schema-builder.ts
@@ -1,31 +1,50 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql/type'
 
-import * as MediaMutations from '../resolvers/file/mutations'
-import * as MediaQuery from '../resolvers/file/query'
+import * as FileMutations from '../resolvers/file/mutations'
+import * as FileQuery from '../resolvers/file/query'
 import * as UserAuthMutations from '../resolvers/user/auth/mutation'
-import { me } from '../resolvers/user/auth/query'
-import { entities, mutations, queries } from './drizzle-schema'
+import * as UserAuthQuery from '../resolvers/user/auth/query'
+import {entities, inputs, mutations, queries} from './drizzle-schema'
 import { FileScalar } from './scalars'
+import {DefaultContext, Maybe} from "@envelop/types";
 
-export default function schemaBuilder(): GraphQLSchema {
+const types = [...Object.values(entities.types), ...Object.values(inputs), FileScalar]
+
+export function getSchema(context : Maybe<DefaultContext>): GraphQLSchema {
+  if (context?.jwt) {
+    return new GraphQLSchema({
+      mutation: new GraphQLObjectType({
+        fields: {
+          ...FileMutations,
+          ...FileQuery,
+          ...mutations,
+        },
+        name: 'Mutation',
+      }),
+      query: new GraphQLObjectType({
+        fields: {
+          ...UserAuthQuery,
+          ...queries,
+        },
+        name: 'Query',
+      }),
+      types
+    })
+  }
+
   return new GraphQLSchema({
     mutation: new GraphQLObjectType({
       fields: {
         ...UserAuthMutations,
-        ...MediaMutations,
-        ...MediaQuery,
-        ...mutations,
       },
       name: 'Mutation',
     }),
     query: new GraphQLObjectType({
       fields: {
-        me,
-        ...queries,
+        ...UserAuthQuery
       },
       name: 'Query',
     }),
-    // todo refine types
-    types: [...Object.values(entities.types), ...Object.values(entities.inputs), FileScalar],
+    types
   })
 }
