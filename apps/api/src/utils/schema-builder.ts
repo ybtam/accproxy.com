937e44db43268ccfe9e3c6cbbb9c2327cd1fcf55
Yi Tam <tam.yi3d@gmail.com>
2025-02-23 14:44:56 +0100

    allow the right token to have access to the whole schema

--- apps/api/src/utils/schema-builder.ts
+++ apps/api/src/utils/schema-builder.ts
@@ -7,44 +7,65 @@ import * as UserAuthQuery from '../resolvers/user/auth/query'
 import {entities, inputs, mutations, queries} from './drizzle-schema'
 import { FileScalar } from './scalars'
 import {DefaultContext, Maybe} from "@envelop/types";
+import {GraphQLFieldConfig, ThunkObjMap} from "graphql/type/definition";
 
 const types = [...Object.values(entities.types), ...Object.values(inputs), FileScalar]
 
-export function getSchema(context : Maybe<DefaultContext>): GraphQLSchema {
-  if (context?.jwt) {
-    return new GraphQLSchema({
-      mutation: new GraphQLObjectType({
-        fields: {
-          ...FileMutations,
-          ...FileQuery,
-          ...mutations,
-        },
-        name: 'Mutation',
-      }),
-      query: new GraphQLObjectType({
-        fields: {
-          ...UserAuthQuery,
-          ...queries,
-        },
-        name: 'Query',
-      }),
-      types
+const schemaBuilder = <TSource, TContext>(props: {mutations: ThunkObjMap<GraphQLFieldConfig<TSource, TContext>>, queries: ThunkObjMap<GraphQLFieldConfig<TSource, TContext>> }) => new GraphQLSchema({
+  mutation: new GraphQLObjectType({
+    fields: props.mutations,
+    name: 'Mutation',
+  }),
+  query: new GraphQLObjectType({
+    fields: props.queries,
+    name: 'Query',
+  }),
+  types
+})
+
+const authSchema = {
+  mutations: {
+    ...FileMutations,
+    ...FileQuery,
+    ...mutations,
+  },
+  queries: {
+    ...UserAuthQuery,
+    ...queries,
+  }
+}
+
+const publicSchema = {
+  mutations: {
+    ...UserAuthMutations,
+  },
+  queries: {
+    ...UserAuthQuery,
+  }
+}
+
+export function getSchema(context: Maybe<{
+  jwt?: object,
+  req?: { headers?: { [key: string]: string } }
+}>): GraphQLSchema {
+  if (
+    context?.req?.headers?.['developer-token'] === process.env.DEV_TOKEN
+  ) {
+    return schemaBuilder({
+      mutations: {
+        ...authSchema.mutations,
+        ...publicSchema.mutations,
+      },
+      queries: {
+        ...authSchema.queries,
+        ...publicSchema.queries,
+      }
     })
   }
 
-  return new GraphQLSchema({
-    mutation: new GraphQLObjectType({
-      fields: {
-        ...UserAuthMutations,
-      },
-      name: 'Mutation',
-    }),
-    query: new GraphQLObjectType({
-      fields: {
-        ...UserAuthQuery
-      },
-      name: 'Query',
-    }),
-    types
-  })
+  if (context?.jwt) {
+    return schemaBuilder(authSchema)
+  }
+
+  return schemaBuilder(publicSchema)
 }
