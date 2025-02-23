7665a96fcf123ae96f938b95322050b0a8a19d81
Yi Tam <tam.yi3d@gmail.com>
2025-02-23 21:02:25 +0100

    create turbo graphql endpoint

--- apps/web/src/app/@login/page.tsx
+++ apps/web/src/app/@login/page.tsx
@@ -1,3 +1,22 @@
+import {signIn} from "@repo/sdk/auth";
+import {Button, Input} from "@repo/ui";
+
 export default function Login() {
-  return <div>Login</div>
+  const handleSubmit = async (formData: FormData) => {
+    'use server'
+
+    await signIn('credentials', {
+      redirectTo: '/',
+      ...Object.fromEntries(formData.entries()),
+    })
+  }
+
+  return <div className={'flex flex-col items-center justify-center gap-4'}>
+    <h1 className={'text-3xl'}>Login</h1>
+    <form action={handleSubmit} className={'flex flex-col gap-4'}>
+      <Input type="email" name="email" placeholder={'Email'}/>
+      <Input type="password" name="password" placeholder={'Password'}/>
+      <Button type="submit">Login</Button>
+    </form>
+  </div>
 }
